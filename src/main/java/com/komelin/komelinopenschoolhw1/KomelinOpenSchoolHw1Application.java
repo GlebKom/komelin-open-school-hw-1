c0f46527da028432a815ca08e1e1d8f5f3307694
Gleb <g.komelin@gmail.com>
2024-05-30 19:34:47 +0300

    Initial commit

---
+++ src/main/java/com/komelin/komelinopenschoolhw1/KomelinOpenSchoolHw1Application.java
@@ -0,0 +1,13 @@
+package com.komelin.komelinopenschoolhw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomelinOpenSchoolHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KomelinOpenSchoolHw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/komelin/komelinopenschoolhw1/KomelinOpenSchoolHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komelin.komelinopenschoolhw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomelinOpenSchoolHw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
